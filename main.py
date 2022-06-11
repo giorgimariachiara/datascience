4a3f1bda445bfce43114a9238380cb9bccfdb826
Maria Chiara <mariachiara.giorgi1@gmail.com>
2022-06-11 13:47:21 +0200

    sss

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# read csv file with pandas 
+
 import pandas as pd
 from pandas import read_csv, Series, DataFrame
 
@@ -17,4 +19,18 @@ publication_df = pd.read_csv("./relational_db/relational_publication.csv", keep_
 
                         },encoding="utf-8")
 
+<<<<<<< Updated upstream
 print(publication_df)
+=======
+#create and connect db
+
+from sqlite3 import connect
+
+with connect("publications.db") as con:
+    # do some operation with the new connection
+    
+    con.commit()  # commit the current transaction to the database
+
+
+print(csv_file)
+>>>>>>> Stashed changes
