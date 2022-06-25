a924d5b256589ac31b665ad36314e713e81b6f85
Maria Chiara <mariachiara.giorgi1@gmail.com>
2022-06-25 14:54:40 +0200

    Update impl2.py

--- impl2.py
+++ impl2.py
@@ -252,7 +252,7 @@ class RelationalQueryProcessor(RelationalProcessor, QueryProcessor):
         rp0.setDbPath(dbPath)
         with connect(rp0.getDbPath()) as con: 
             con.commit()
-            VenuesDF = read_sql("SELECT * FROM Venueid WHERE publisher= " + str(publisher), con)
+            VenuesDF = read_sql("SELECT * FROM Venueid WHERE publisher= " + publisher, con)
         return VenuesDF 
     
 
@@ -297,7 +297,7 @@ gqp = GenericQueryProcessor()
 #print(rqp.getDbPath())
 #RelationalQueryProcessor.setDbPath(dbPath)
 #print(RelationalQueryProcessor.getDbPath())
-#print(gqp.getPublicationsByAuthorId("0000-0001-8686-0017"))
+print(gqp.getPublicationsByAuthorId("0000-0001-8686-0017"))
 
 #print(gqp.getPublicationAuthors("doi:10.1162/qss_a_00023"))
-print(gqp.getVenuesByPublisherId('crossref:281'))
+#print(gqp.getVenuesByPublisherId(publisher="crossref:281"))
