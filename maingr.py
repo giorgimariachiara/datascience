1e32d8f3e9729981fb29a6449227bfd6791e279a
Maria Chiara <mariachiara.giorgi1@gmail.com>
2022-07-03 15:34:16 +0200

    m

--- maingr.py
+++ maingr.py
@@ -91,9 +91,13 @@ my_graph.add((subj, publicationYear, Literal(row["publication_year"])))
 my_graph.add((subj, event, Literal(row["event"])))
 my_graph.add((subj, organization, Literal(row["publisher"])))
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 my_graph.add((subj, publicationVenue, Literal(row["publication_venue"])))   
 
 #venue_internal_id[row["publication venue"]] questo è quello che ha mesos Peroni bisogna ccapire perchè 
+
+my_graph.add((subj, publicationVenue, publications_internal_id[row["publication_venue"]]))   #venue_internal_id[row["publication venue"]] questo è quello che ha mesos Peroni bisogna ccapire perchè 
+>>>>>>> Stashed changes
 =======
 my_graph.add((subj, publicationVenue, publications_internal_id[row["publication_venue"]]))   #venue_internal_id[row["publication venue"]] questo è quello che ha mesos Peroni bisogna ccapire perchè 
 >>>>>>> Stashed changes
@@ -114,8 +118,3 @@ for triple in my_graph.triples((None, None, None)): #none none none means that i
 # Once finished, remeber to close the connection
 store.close()
 
-print(len(my_graph))
-<<<<<<< Updated upstream
-print(my_graph)
-=======
->>>>>>> Stashed changes
