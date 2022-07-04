08b95f50a91441460e267d723b4b6288ae30fe72
Luca Scotti <scottiluca0@gmail.com>
2022-07-04 15:01:59 +0200

    s

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from locale import normalize
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 from pandas import DataFrame, merge 
+=======
+
+
+>>>>>>> Stashed changes
 =======
 
 
@@ -40,6 +45,8 @@ from pandas import read_csv, Series, read_json
 from impl2 import GenericQueryProcessor
 #from impl2 import RelationalDataProcessor, RelationalQueryProcessor 
 from impl2 import  RelationalQueryProcessor 
+
+
 
 
 
