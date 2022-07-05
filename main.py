0f64fd8dcabd243a2e1da33daa4e6e9ab7fb28d0
Luca Scotti <scottiluca0@gmail.com>
2022-07-05 16:52:28 +0200

    Revert "s"
    This reverts commit 1f301aae0cb9867440bfaf4ed0e6b70679680961.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from locale import normalize
 # read csv file with pandas
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 # <<<<<<< Updated upstream
 # <<<<<<< Updated upstream
 # <<<<<<< Updated upstream
@@ -32,10 +31,6 @@ from locale import normalize
 
 
 >>>>>>> Stashed changes
-=======
-
-
->>>>>>> Stashed changes
 from operator import index
 from numpy import index_exp
 from pandas import merge 
@@ -50,8 +45,6 @@ from pandas import read_csv, Series, read_json
 from impl2 import GenericQueryProcessor
 #from impl2 import RelationalDataProcessor, RelationalQueryProcessor 
 from impl2 import  RelationalQueryProcessor 
-
-
 
 
 
