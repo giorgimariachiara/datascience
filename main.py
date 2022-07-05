403687f9d768be8a1428c39669c5cdf7a6692872
Luca Scotti <scottiluca0@gmail.com>
2022-07-05 16:45:30 +0200

    s

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from locale import normalize
 # read csv file with pandas
+<<<<<<< Updated upstream
 # <<<<<<< Updated upstream
 # <<<<<<< Updated upstream
 # <<<<<<< Updated upstream
@@ -26,6 +27,10 @@ from locale import normalize
 
 
 # >>>>>>> Stashed changes
+=======
+
+
+>>>>>>> Stashed changes
 from operator import index
 from numpy import index_exp
 from pandas import merge 
@@ -40,6 +45,8 @@ from pandas import read_csv, Series, read_json
 from impl2 import GenericQueryProcessor
 #from impl2 import RelationalDataProcessor, RelationalQueryProcessor 
 from impl2 import  RelationalQueryProcessor 
+
+
 
 
 
