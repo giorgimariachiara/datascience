caf538987d81aa5dc066c053891849a097af2973
Luca Scotti <scottiluca0@gmail.com>
2022-07-05 16:55:13 +0200

    s

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from locale import normalize
 # read csv file with pandas
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 # <<<<<<< Updated upstream
 # <<<<<<< Updated upstream
@@ -31,6 +32,9 @@ from locale import normalize
 
 
 >>>>>>> Stashed changes
+=======
+from pandas import DataFrame, concat, merge 
+>>>>>>> parent of f50355e (Update main.py)
 from operator import index
 from numpy import index_exp
 from pandas import merge 
@@ -42,29 +46,7 @@ from pprint import pprint
 from pandas import read_sql
 import pandas as pd
 from pandas import read_csv, Series, read_json
-from impl2 import GenericQueryProcessor
 #from impl2 import RelationalDataProcessor, RelationalQueryProcessor 
-from impl2 import  RelationalQueryProcessor 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -97,6 +79,28 @@ crossref = json_doc.get("publishers")
 id_and_name = crossref.values()
 organization_df = pd.DataFrame(id_and_name)
 
+organization_internal_id = []
+for idx, row in organization_df.iterrows():
+    organization_internal_id.append("organization-" + str(idx))
+organization_df.insert(0, "OrganizationId", Series(organization_internal_id, dtype="string"))
+
+#----------------------------------------
+
+# Venue DataFrame
+
+venue_df = publication_df[["id", "publication_venue", "publisher"]]
+venue_df = merge(venue_df, organization_df, left_on="publisher", right_on="id")
+venue_df = venue_df[["publication_venue", "OrganizationId"]]
+venue_df.drop_duplicates(subset= ["publication_venue", "OrganizationId"], inplace = True)
+venue_df = venue_df.reset_index()
+
+venue_internal_id = []
+for idx, row in venue_df.iterrows():
+    venue_internal_id.append("venue-" + str(idx))
+venue_df.insert(0, "VenueId", Series(venue_internal_id, dtype="string"))
+venue_df.drop("index", axis=1, inplace = True)
+
+print(venue_df)
 
 #----------------------------------------
 
@@ -160,37 +164,15 @@ person_df = pd.DataFrame({
 #----------------------------------------
 
 
-# Book chapter DataFrame
-
-
-book_chapter_df = publication_df.query("type == 'book-chapter'")
-book_chapter_df = book_chapter_df[["id", "publication_year", "title", "publication_venue", "chapter"]]
-
-book_chapter_df = book_chapter_df.rename(columns={"id":"doi"})
-pd.set_option("display.max_colwidth", None, "display.max_rows", None)
-
-
-#----------------------------------------
-
-
-# Proceedings paper DataFrame
-
-Proceedings_paper_df = publication_df.query("type == 'proceeding-paper'")
-Proceedings_paper_df = Proceedings_paper_df[["id", "publication_year", "title", "publication_venue", "issue", "volume"]]
-Proceedings_paper_df = Proceedings_paper_df.rename(columns={"id":"doi"})
-pd.set_option("display.max_colwidth", None, "display.max_rows", None)
-
-
-#----------------------------------------
-
-
 # Proceedings DataFrame
 
 proceedings_df = publication_df.query("venue_type =='proceedings'")
 proceedings_df = proceedings_df[["id", "publication_venue", "publisher", "event"]]
-
 pd.set_option("display.max_colwidth", None, "display.max_rows", None)
-
+proceedings_df= merge(venue_df, proceedings_df, left_on="publication_venue", right_on="publication_venue")
+proceedings_df = proceedings_df[["id", "VenueId", "publisher", "event"]]
+proceedings_df = proceedings_df.rename(columns={"VenueId":"publication_venue"})
+proceedings_df = proceedings_df.rename(columns={"id":"doi"})
 
 #----------------------------------------
 
@@ -215,6 +197,9 @@ book_df = publication_df.query("venue_type =='book'")
 book_df= book_df[["id", "publication_venue", "publisher"]]
 pd.set_option("display.max_colwidth", None, "display.max_rows", None)
 book_df = book_df.rename(columns={"id":"doi"})
+book_df= merge(venue_df, book_df, left_on="publication_venue", right_on="publication_venue")
+book_df = book_df[["doi", "VenueId", "publisher"]]
+book_df = book_df.rename(columns={"VenueId":"publication_venue"})
 
 
 #----------------------------------------
@@ -225,6 +210,12 @@ book_df = book_df.rename(columns={"id":"doi"})
 journal_df = publication_df.query("venue_type =='journal'")
 journal_df= journal_df[["id", "publication_venue", "publisher"]]
 pd.set_option("display.max_colwidth", None, "display.max_rows", None)
+journal_df = journal_df.rename(columns={"id":"doi"})
+journal_df= merge(venue_df, journal_df, left_on="publication_venue", right_on="publication_venue")
+journal_df = journal_df[["doi", "VenueId", "publisher"]]
+journal_df = journal_df.rename(columns={"VenueId":"publication_venue"})
+
+
 
 
 #----------------------------------------
@@ -235,17 +226,36 @@ pd.set_option("display.max_colwidth", None, "display.max_rows", None)
 journal_article_df = publication_df.query("type =='journal-article'")
 journal_article_df = journal_article_df[["id", "publication_year", "title", "publication_venue", "issue", "volume"]]
 pd.set_option("display.max_colwidth", None, "display.max_rows", None)
-journal_article_df = journal_article_df.rename(columns={"id":"doi"})
-jaDF = merge(journal_article_df, journal_df, left_on="doi", right_on="id")
+#journal_article_df = journal_article_df.rename(columns={"id":"doi"})
+journal_article_df= merge(venue_df, journal_article_df, left_on="publication_venue", right_on="publication_venue")
+journal_article_df = journal_article_df[["VenueId", "id", "publication_year", "title", "issue", "volume"]]
+journal_article_df = journal_article_df.rename(columns={"VenueId":"publication_venue"})
+
+#----------------------------------------
+
+# Book chapter DataFrame
 
 
-#print(df_joined)
+book_chapter_df = publication_df.query("type == 'book-chapter'")
+book_chapter_df = book_chapter_df[["id", "publication_year", "title", "publication_venue", "chapter"]]
 
-#journal_article_df = journal_article_df[["id", "publication_year", "title", "publication_venue", "issue", "volume"]]
+book_chapter_df = book_chapter_df.rename(columns={"id":"doi"})
+pd.set_option("display.max_colwidth", None, "display.max_rows", None)
+book_chapter_df= merge( venue_df, book_chapter_df, left_on="publication_venue", right_on="publication_venue")
+book_chapter_df = book_chapter_df[["VenueId", "doi", "title", "publication_year", "chapter"]]
+book_chapter_df = book_chapter_df.rename(columns={"VenueId":"publication_venue"})
 
+#----------------------------------------
+# Proceedings paper DataFrame
 
-# Venue DataFrame
+Proceedings_paper_df = publication_df.query("type == 'proceeding-paper'")
+Proceedings_paper_df = Proceedings_paper_df[["id", "publication_year", "title", "publication_venue", "issue", "volume"]]
+Proceedings_paper_df = Proceedings_paper_df.rename(columns={"id":"doi"})
+pd.set_option("display.max_colwidth", None, "display.max_rows", None)
+Proceedings_paper_df= merge(venue_df, Proceedings_paper_df, left_on="publication_venue", right_on="publication_venue")
 
+#----------------------------------------
+#Venues ext Dataframe
 Venue=json_doc["venues_id"]
 
 doi_list = []
@@ -262,20 +272,22 @@ venues = pd.DataFrame({
     
 })
 
-venue_df = publication_df[["id", "publication_venue", "publisher"]]
+venue_ext_dfjournalart = merge(journal_article_df, venues, left_on="id", right_on="doi")
+venue_ext_dfjournalart = venue_ext_dfjournalart[["publication_venue", "issn_isbn"]]
+venue_ext_dfbookchapter = merge(book_chapter_df, venues, left_on="doi", right_on="doi")
+venue_ext_dfbookchapter = venue_ext_dfbookchapter[["publication_venue", "issn_isbn"]]
+
+print(venue_ext_dfbookchapter)
 
 
-df_joinVV = merge(venues, venue_df, left_on="doi", right_on = "id") 
+#df_joinVV = merge(venues, venue_df, left_on="doi", right_on = "id") 
 
-venue_df = df_joinVV[["id", "issn_isbn", "publication_venue", "publisher"]]
-venue_df = venue_df.rename(columns={"issn/isbn":"issn_isbn"})
-#venue_df = venue_df[["issn_isbn", "id", "publication_venue", "publisher"]]
+#venue_df = df_joinVV[["id", "issn_isbn", "publication_venue", "publisher"]]
+#venue_df = venue_df.rename(columns={"issn/isbn":"issn_isbn"})
 
+#Venues_ext_df = 
 
-#print(venue_df)
-
-#----------------------------------------
-
+"""
 
 # Populate the SQL database 
 with connect("publication.db") as con:
@@ -300,7 +312,7 @@ with connect("publication.db") as con:
   
 
     con.commit()
-
+"""
  
 #result_q1 = generic.getPublicationsPublishedInYear(2020)
 
