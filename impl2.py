ae6959f1c6a00bb417dba03d361eab50bfa13d04
Maria Chiara <mariachiara.giorgi1@gmail.com>
2022-07-12 09:17:48 +0200

    ,

--- impl2.py
+++ impl2.py
@@ -311,14 +311,6 @@ class TriplestoreProcessor(object):
         
     def setEndpointUrl(self, url):
         self.endpointUrl = url
-<<<<<<< Updated upstream
-=======
-    
-    def getEndpointUrl(self):
-        return self.endpointUrl
-        
-           
->>>>>>> Stashed changes
     
     def getEndpointUrl(self):
         return self.endpointUrl
