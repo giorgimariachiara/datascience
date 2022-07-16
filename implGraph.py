ac07c55b2b71ec2cfd6bfb0a28862ba76d458a59
Luca Scotti <scottiluca0@gmail.com>
2022-07-16 20:00:06 +0200

    Update implGraph.py

--- implGraph.py
+++ implGraph.py
@@ -1,27 +1,27 @@
-from implRel import *
-import rdflib
+# from implRel import *
+# import rdflib
 
 
-endpointUrl = 'http://127.0.0.1:9999/blazegraph/sparql'
+# endpointUrl = 'http://127.0.0.1:9999/blazegraph/sparql'
 
-class TripleQueryProcessor(TriplestoreProcessor, QueryProcessor):
-    def getPublicationsPublishedInYear(self, py):
-        g = rdflib.Graph()
-        g.parse("http://127.0.0.1:9999/blazegraph/sparql")
-        #tp0 = TriplestoreProcessor() 
-        #tp0.setEndpointUrl(endpointUrl)
-        #tp0.getEndpointUrl()
-        query = """ 
-        SELECT *
-        WHERE {
-            SERVICE <http://127.0.0.1:9999/blazegraph/sparql> {
-            ?s rdf:type schema:ScholarlyArticle.
-            ?s schema:datePublished "2021".
-            ?s ?p ?o }
-        }
-        """
-        ciao = g.query(query)
-        print(ciao)
+# class TripleQueryProcessor(TriplestoreProcessor, QueryProcessor):
+#     def getPublicationsPublishedInYear(self, py):
+#         g = rdflib.Graph()
+#         g.parse("http://127.0.0.1:9999/blazegraph/sparql")
+#         #tp0 = TriplestoreProcessor() 
+#         #tp0.setEndpointUrl(endpointUrl)
+#         #tp0.getEndpointUrl()
+#         query = """ 
+#         SELECT *
+#         WHERE {
+#             SERVICE <http://127.0.0.1:9999/blazegraph/sparql> {
+#             ?s rdf:type schema:ScholarlyArticle.
+#             ?s schema:datePublished "2021".
+#             ?s ?p ?o }
+#         }
+#         """
+#         ciao = g.query(query)
+#         print(ciao)
         
         
         
